827e42737881ff799fe61bdb5778cc91a44511c4
Alma <isakvent@outlook.com>
2023-10-16 23:37:44 +0200

    added a function that can detect and differenciate legal input

--- count_for_expression.py
+++ count_for_expression.py
@@ -1,10 +1,12 @@
 ''' docstring '''
-LEGAL_LETTERS_VAR = ['a', 'b', 'c', 'x', 'y', 'z', '(', ')']
-LEGAL_LETTERS_OPERATORS = ['+', '-', '*', '/', '^', 'sqrt']
+LEGAL_LETTERS_VAR = ['a', 'b', 'c', 'x', 'y', 'z',]
+LEGAL_LETTERS_OPERATORS = ['+', '-', '*', '/', '^', 'sqrt',]
+LEGAL_INTEGERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 ''' docstring '''
 MY_STRING = []
 INPUT_EXPRESSION = None
+
 
 def update_my_string():
     ''' docstring '''
@@ -12,7 +14,10 @@ def update_my_string():
     new_string = input(str("type your expression: "))
     new_string = new_string.replace(" ", "").lower()
     return new_string
+
+
 INPUT_EXPRESSION = update_my_string()
+
 
 def append_my_string():
     ''' docstring '''
@@ -20,6 +25,22 @@ def append_my_string():
         MY_STRING.append(i)
         print(MY_STRING)
 
+
+def verify_my_string():
+    for i in MY_STRING:
+        print(i)
+        if i in LEGAL_INTEGERS:
+            print("integer")
+        elif i in LEGAL_LETTERS_VAR:
+            print("variable")
+        elif i in LEGAL_LETTERS_OPERATORS:
+            print("operator")
+        else:
+            print("illegal")
+
+
 append_my_string()
+verify_my_string()
+verify_my_string()
 
 # End-of-file (EOF)
