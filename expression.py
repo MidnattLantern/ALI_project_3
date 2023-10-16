08b7bbb920b99727c73744c4b2216ddb7c8b7ce4
Alma <isakvent@outlook.com>
2023-10-16 14:35:53 +0200

    Added End of file to python documents

---
+++ expression.py
@@ -0,0 +1,24 @@
+"""
+docstring
+"""
+my_expression = {
+    "coefficient": None,
+    "variable": None,
+    "exponent": None,
+}
+
+print(my_expression)
+
+
+def update_my_expression(coefficient, variable, exponent):
+    """
+    docstring
+    """
+    my_expression["coefficient"]: coefficient
+    my_expression["variable"]: variable
+    my_expression["exponent"]: exponent
+
+
+update_my_expression(2, "x", 3)
+
+# End-of-file (EOF)
