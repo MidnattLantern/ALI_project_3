fd463bbfb019d75c2567fcb34605ef549c62330e
Alma <isakvent@outlook.com>
2023-10-17 16:01:58 +0200

    run.py can read two programs: derivate_guide and expression_input

--- expression_input.py
+++ expression_input.py
@@ -1,13 +1,15 @@
-''' docstring '''
+''' Following characters can be used '''
 LEGAL_LETTERS_VAR = ['a', 'b', 'c', 'x', 'y', 'z']
 LEGAL_LETTERS_OPERATORS = ['+', '-', '*', '/', '^', 'sqrt']
 LEGAL_INTEGERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
-''' docstring '''
+''' all things filtering messy input into organised libraries '''
 class MyPolymonial:
-    ''' docstring '''
+    ''' all_index and input_expresson may store illegal characters,
+     but int_index, var_index, opr_index wont, and only those will
+     be used for following steps '''
     def __init__(self, input_expression):
-        # comment
+        # The "organised libraries"
         self.all_index = []
         self.int_index = []
         self.var_index = []
@@ -15,17 +17,17 @@ class MyPolymonial:
         self.input_expression = input_expression
 
     def update_input_expression(self, new_index):
-        ''' docstring '''
+        ''' Captured from user input in later code '''
         self.input_expression = new_index
 
     def append_all_index(self):
-        ''' docstring '''
+        ''' Has to be done before verification '''
         for i in self.input_expression:
             self.all_index.append(i)
             print(self.all_index)
 
     def verify_all_index(self):
-        ''' docstring '''
+        ''' Verification has to be done after append_all_index '''
         for i in self.all_index:
             if i in LEGAL_INTEGERS:
                 print(i + ' is an integer')
@@ -48,7 +50,7 @@ class MyPolymonial:
                 continue
 
     def print_index(self):
-        ''' docstring '''
+        ''' user's feedback '''
         print(self.all_index)
         print(self.int_index)
         print(self.var_index)
--- run.py
+++ run.py
@@ -1,10 +1,32 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+''' docstring '''
+
+run_input = ['1', 'input']
+derivate_guide = ['2', 'derivate_guide', 'derivate guide', 'derivate help']
+
+APPLICATION = None
+
+print("Type the program you want to use:")
+print("1: input")
+print("2: derivate guide")
+
+def run(APPLICATION):
+    ''' docstring '''
+    if APPLICATION in run_input:
+        from expression_input import MyPolymonial
+        new_polinomial = MyPolymonial("")
+        new_polinomial.update_input_expression(str(input("input: ")))
+        new_polinomial.append_all_index()
+        new_polinomial.verify_all_index()
+        new_polinomial.print_index()
+    elif APPLICATION in derivate_guide:
+        f = open('derivate_guide.txt', encoding='utf-8')
+        lines = f.read()
+        f.close()
+        print(lines)
+    else:
+        print("try again")
+
+APPLICATION = input("Run: ")
+run(APPLICATION)
 
 # End-of-file (EOF)
-
-f = open('derivate_guide.txt')
-lines = f.read()
-f.close()
-print(lines)
