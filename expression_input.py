efeade22e9bb130ed591b2c0174fd8f6155a2bbd
Alma <isakvent@outlook.com>
2023-10-17 02:56:59 +0200

    Enable string input in OOS version of input validation

--- expression_input.py
+++ expression_input.py
@@ -1,94 +1,73 @@
-# user input will automatically be proccessed to a readable equation, regardless how messy and nonselsical it may be
-"""
-- any variable, such as x y z will always have
- a coefficient and an exponent,
-if neither is given, they'll automatically be assigned as 1
-- any non integer will be automatically seperated
-"""
+''' docstring '''
+LEGAL_LETTERS_VAR = ['a', 'b', 'c', 'x', 'y', 'z']
+LEGAL_LETTERS_OPERATORS = ['+', '-', '*', '/', '^', 'sqrt']
+LEGAL_INTEGERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
-# "libraries" or "library"
-sliced_expression = []
-sliced_expression_int_target = []
-sliced_expression_str_target = []
-sliced_expression_type = []
-monomial_dictionary = {}
+''' docstring '''
+class MyPolymonial:
+    ''' docstring '''
+    def __init__(self, input_expression):
+        # comment
+        self.all_index = []
+        self.int_index = []
+        self.var_index = []
+        self.opr_index = []
+        self.input_expression = input_expression
 
-# edit of expression as string
-print("Note: integer following ^ will be an exponent")
-input_expression = str(input("Enter expression: "))
-# filtering out CAPITAL and spaces
-input_expression = input_expression.replace(" ", "").lower()
-print("Expression: "+ input_expression)
+    def update_input_expression(self, new_index):
+        ''' docstring '''
+        self.input_expression = new_index
 
-# sliced_expression
-for i in input_expression:
-    sliced_expression.append(i)
+    def print_index(self):
+        print(self.all_index)
+        print(self.int_index)
+        print(self.var_index)
+        print(self.opr_index)
+        print(self.input_expression)
 
-print("------")
-for i in sliced_expression:
-    try:
-        print(int(i))
-        sliced_expression_int_target.append(i)
-        sliced_expression_str_target.append(None)
-    except:
-        print(i+ " is not an integer")
-        sliced_expression_str_target.append(i)
-        sliced_expression_int_target.append(None)
-print("------")
+new_polinomial = MyPolymonial("test")
 
-print("------")
-print("all: "+ str(sliced_expression))
-#print(range(len(sliced_expression)))
-print("int: "+ str(sliced_expression_int_target))
-#print(range(len(sliced_expression_int_target)))
-print("str: "+ str(sliced_expression_str_target))
-#print(range(len(sliced_expression_str_target)))
-print("------")
+new_polinomial.update_input_expression(str(input("input: ")))
 
-# Identifying strings and integers using libraries
-print("------")
-for i in range(len(sliced_expression)):
-    if sliced_expression[i] == sliced_expression_int_target[i]:
-        print(str(sliced_expression[i])+ " is an integer")
-        sliced_expression_type.append(int)
-    elif sliced_expression[i] == sliced_expression_str_target[i]:
-        print(str(sliced_expression[i])+ " is a string")
-        sliced_expression_type.append(str)
-    else:
-        print("error: unable to identify integer or string for"+ str(sliced_expression[i]))
-print("------")
+print(new_polinomial.input_expression)
+
+
+# End-of-file (EOF)
 
 '''
-- An expression, such as 12x, would in this case be:
-1 as index [0], 2 as index [1], and x as index[2]
-- Values may have more than one digit, so the next
-index will be appended to the string.
-- Variables in math are always single, so if the next
-or first or next index is a string, a monomial will be
-generated using the given values up until that point.
-- monomial_index keeps track how many monomials there
-should exist.
-'''
-print("------")
-write_monomial = ""
-monomial_index = 0
-for x in range(len(sliced_expression_type)):
-    if sliced_expression_type[x] == int:
-        write_monomial += sliced_expression[x]
-    elif sliced_expression_type[x] == str:
-        # Detecting operators
-        if sliced_expression_str_target[x] not in ["^", "+", "-", "*", "/", "sqrt"]:
-            write_monomial += sliced_expression[x]
-            # Create monomial, reset write_monomial, then move on to the next
-            monomial_dictionary[monomial_index] = write_monomial
-            monomial_index += 1
-            write_monomial = ""
-    # Detecting when reaching the final input character
-    if x == len(sliced_expression[x]):
-        print("final object")
-        if sliced_expression_type[x] == int:
-            print("final object is an integer")
-print("total indexes: "+ str(len(sliced_expression)))
-print(monomial_dictionary)
+def append_all_index():
 
-print("------")
+    for i in input_expression:
+        all_index.append(i)
+        print(all_index)
+
+
+def verify_all_index():
+
+    for i in all_index:
+        if i in LEGAL_INTEGERS:
+            print(i + " is an integer")
+            int_index.append(int(i))
+            var_index.append(None)
+            opr_index.append(None)
+        elif i in LEGAL_LETTERS_VAR:
+            print(i + " is a variable")
+            int_index.append(None)
+            var_index.append(i)
+            opr_index.append(None)
+        elif i in LEGAL_LETTERS_OPERATORS:
+            print(i + " is an operator")
+            int_index.append(None)
+            var_index.append(None)
+            opr_index.append(i)
+        else:
+            print(i + " is illegal and will be discarded")
+            continue
+    print("Integer index: " + str(int_index))
+    print("Variable index: " + str(var_index))
+    print("Operator index: " + str(opr_index))
+'''
+
+
+
+# End-of-file (EOF)
--- test2.py
+++ test2.py
@@ -1,4 +1,18 @@
-myVar = str("sas")
+class MyObject:
+    def __init__(self, name):
+        self.name = name
 
-for i in myVar:
-    print(i)
+    def update_name(self, new_name):
+        self.name = new_name
+
+# Create an instance of the class
+my_instance = MyObject("John")
+
+# Print the initial name
+print(f"Initial Name: {my_instance.name}")
+
+# Update the name using the method
+my_instance.update_name("Jane")
+
+# Print the updated name
+print(f"Updated Name: {my_instance.name}")
