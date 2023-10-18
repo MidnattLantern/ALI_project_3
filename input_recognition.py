80ab68aa88fdd25db8456efb26762d7aba3cb8b6
Alma <isakvent@outlook.com>
2023-10-18 18:46:01 +0200

    recoognition can append a monomonial without following operator

--- input_recognition.py
+++ input_recognition.py
@@ -1,8 +1,8 @@
-all_index = [2, 5, 'x', '^', 5,'+','7', '-']
-int_index = [2, 5, None, None, 5, None, '7', None]
-var_index = [None, None, 'x', None, None, None, None, None]
-opr_index = [None, None, None, '^', None, '+', None, '-']
-filter = []
+all_index = [2, 5, 'x', '^', 5,'+','7', '+', '2', '-']
+int_index = [2, 5, None, None, 5, None, 7, None, 2, None]
+var_index = [None, None, 'x', None, None, None, None, None, None, None]
+opr_index = [None, None, None, '^', None, '+', None, '+', None, '-']
+monomonial = ['+',]
 
 dict_index = 0
 
@@ -23,22 +23,33 @@ print_all()
 
 def split():
     my_dict.update({
-        dict_index: [''.join(filter)]
+        dict_index: [''.join(monomonial)]
     })
     print(my_dict)
 
 print('------')
-# the , _ excludes the value
+# generating each monomonial
 for i, _ in enumerate(all_index):
+    print(i+ 1)
     if int_index[i] != None:
-        filter.append(str(int_index[i]))
-    if var_index[i] != None:
-        filter.append(str(var_index[i]))
-    if opr_index[i] != None:
+        monomonial.append(str(int_index[i]))
+
+    elif var_index[i] != None:
+        monomonial.append(str(var_index[i]))
+
+    elif opr_index[i] != None:
         if opr_index[i] == '^':
-            filter.append(str(opr_index[i]))
+            monomonial.append(str(opr_index[i]))
+
         else:
             split()
             dict_index += 1
-            filter.clear()
-            filter.append(str(opr_index[i]))
+            monomonial.clear()
+            monomonial.append(str(opr_index[i]))
+
+    # cannot be elif !!
+    if int(i+ 1) == int(len(all_index)):
+        split()
+        monomonial.append(str(opr_index[i]))
+
+print(my_dict)
