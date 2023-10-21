51b7004cd87b50678f1f8050226ccebecf7beb5c
Alma <isakvent@outlook.com>
2023-10-21 14:14:33 +0200

    2p equation prints the equation in a UX friendly manner

--- experiment2.py
+++ experiment2.py
@@ -28,17 +28,32 @@ class My2pEquation():
         print(myVar)
         self.indx_x1 = int(input("Point x1: "))
         self.x1 = myVar[self.indx_x1][0]
+        print("set x1 to: "+ str(self.x1))
+
         self.indx_x2 = int(input("Point x2: "))
         self.x2 = myVar[self.indx_x2][0]
+        print("set x2 to: "+ str(self.x2))
+
         self.indx_y1 = int(input("Point y1: "))
         self.y1 = myVar[self.indx_y1][0]
+        print("set y1 to: "+ str(self.y1))
+
         self.indx_y2 = int(input("Point y2: "))
         self.y2 = myVar[self.indx_y2][0]
+        print("set y2 to: "+ str(self.y2))
+
         print('------')
 
     def reveal_2p_equation(self, x1, x2, y1, y2):
         ''' checking that all are assigned a value '''
         if all(var is not None for var in [x1, x2, y1, y2]):
+
+            print('')
+            print('    '+ str(y2)+ ' - '+ (str(y1)))
+            print('k = ------------ = ')
+            print('    '+ str(x2)+ ' - '+ (str(x1)))
+            print('')
+
             try:
                 k = (int(y2) - int(y1)) / ( int(x2) - int(x1))
                 print("k value for 2p equation is: "+ str(k))
