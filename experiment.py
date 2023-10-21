68f37a02631fc5d703f6add47a16adb4277c1e1d
Alma <isakvent@outlook.com>
2023-10-21 13:12:10 +0200

    index input takes the dictionary item instead of the index value for experimental 2p equation

--- experiment.py
+++ experiment.py
@@ -14,20 +14,46 @@ myVar = input_recognition.return_my_polymonial()
 
 print("")
 print("--- 2-point Equation ---")
-print("Use the index for any value you've assigned,")
-print("values containing a variable will be rejected")
-print("Your values:")
+print("Use the index for any monomonial you've assigned,")
+print("monomonials containing a variable will be rejected")
+print("Your polymonials:")
 print(myVar)
-x1 = int(input("Point x1: "))
-x2 = int(input("Point x2: "))
-y1 = int(input("Point y1: "))
-y2 = int(input("Point y2: "))
-print('------')
+indx_x1 = int(input("Point x1: "))
+indx_x2 = int(input("Point x2: "))
+indx_y1 = int(input("Point y1: "))
+indx_y2 = int(input("Point y2: "))
+
+
+def update_2p(indx_x1, indx_x2, indx_y1, indx_y2):
+    global x1
+    global x2
+    global y1
+    global y2
+    x1 = myVar[indx_x1][0]
+    x2 = myVar[indx_x2][0]
+    y1 = myVar[indx_y1][0]
+    y2 = myVar[indx_y2][0]
+
+print(x1)
+print(x2)
+print(y1)
+print(y2)
+print("------")
+update_2p(indx_x1, indx_x2, indx_y1, indx_y2)
+print(x1)
+print(x2)
+print(y1)
+print(y2)
+print("------")
 
 def my_2p_equation(x1, x2, y1, y2):
+
     if all(var is not None for var in [x1, x2, y1, y2]):
-        k = ( y2 - y1) / ( x2 - x1)
-        print("k value for 2p equation is: "+ str(k))
+        try:
+            k = ( int(y2) - int(y1)) / ( int(x2) - int(x1))
+            print("k value for 2p equation is: "+ str(k))
+        except ZeroDivisionError:
+            print("k value for 2p equation is: 0")
     else:
         print("error: either of the point(s) are unassigned")
 
--- experiment2.py
+++ experiment2.py
@@ -1,18 +1,47 @@
-myVar = {
-    0: ['a', 'b', 'c'],
-    1: ['1', '2', '3'],
-    2: ['x', 'y', 'z'],
-    3: ['7', '8', '9', 'sas']
-}
+from input_recognition import InputRecognition
 
+class My2pEquation():
+    ''' docstring '''
+    def __init__(self):
+        self.x1 = None
+        self.x2 = None
+        self.y1 = None
+        self.y2 = None
 
-print(myVar)
-x1 = int(input())
-x2 = int(input())
-y1 = int(input())
-y2 = int(input())
-print('------')
-print(myVar[x1][0])
-print(myVar[x2][0])
-print(myVar[y1][0])
-print(myVar[y2][0])
+    def my_var(self):
+        ''' docstring '''
+        self.myVar = {
+
+        }
+
+    def assign_points(self):
+        ''' docstring '''
+        print("")
+        print("--- 2-point Equation ---")
+        print("Use the index for any monomonial you've assigned,")
+        print("monomonials containing a variable will be rejected")
+        print("Your polymonials:")
+        print(myVar)
+        self.x1 = int(input("Point x1: "))
+        self.x2 = int(input("Point x2: "))
+        self.y1 = int(input("Point y1: "))
+        self.y2 = int(input("Point y2: "))
+        print('------')
+
+    def reveal_2p_equation(self, x1, x2, y1, y2):
+        ''' checking that all are assigned a value '''
+        if all(var is not None for var in [x1, x2, y1, y2]):
+            try:
+                k = ( y2 - y1) / ( x2 - x1)
+                print("k value for 2p equation is: "+ str(k))
+            except ZeroDivisionError:
+                print("k value for 2p equation is: 0")
+        else:
+            print("error: either of the point(s) are unassigned")
+
+run_2p_equation = My2pEquation()
+input_recognition = InputRecognition()
+input_recognition.run_input()
+myVar = input_recognition.return_my_polymonial()
+run_2p_equation.assign_points()
+print(run_2p_equation.x1)
